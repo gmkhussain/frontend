6f79e684ef206cba5ceae40132765290a8dfc61d
hussaingolpik <hussaingolpik@gmail.com>
2015-11-18 18:18:17 -0800

    Tooltip 2

--- js/animateme.js
+++ js/animateme.js
@@ -417,6 +417,28 @@ jQuery('img').error(function(){
 
 
 
+
+
+/////////TOOL TIP///////
 $('body').tooltip({    
     selector: "a[rel=tooltip]"
 })
+
+
+// Enabling Popover Example //
+$(function(){
+    // Enabling Popover Example 1 - HTML (content and title from html tags of element)
+    $("[data-toggle=popover]").popover();
+
+    // Enabling Popover Example 2 - JS (hidden content and title capturing)
+    $("#popoverExampleTwo").popover({
+        html : true, 
+        content: function() {
+          return $('#popoverExampleTwoHiddenContent').html();
+        },
+        title: function() {
+          return $('#popoverExampleTwoHiddenTitle').html();
+        }
+    });
+
+});
