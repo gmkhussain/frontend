17abd8f1c9b810994ebe031936293c575b5f8e2d
Hussain <gmk.hussain@yahoo.com>
2016-01-22 13:25:28 -0800

    :rocket: Scroll To Top

--- js/kscript.js
+++ js/kscript.js
@@ -323,7 +323,7 @@ jQuery(document).ready(function(){
 
 
 
-/*
+
 //Scroll to TOP//
 jQuery(document).ready(function(){
 	//Check to see if the window is top if not then display button
@@ -340,9 +340,9 @@ jQuery(document).ready(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
-	
+
 });
-*/
+
 
 
 
