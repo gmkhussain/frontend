f03899e59ad809e6a3c9e121a0c023b77b9de397
Hussain <gmk.hussain@yahoo.com>
2017-07-12 18:07:48 -0700

    :heavy_plus_sign: :star2: { Customized.js } added for project specific scripting

---
+++ js/customized.js
@@ -0,0 +1,29 @@
+console.log("* customized.js exist *");
+
+
+var findSwiper = $('body').find('div');
+
+if( findSwiper.hasClass(".swiper-container") ){ // .hasClass() returns BOOLEAN true/false
+
+  		var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+                    slidesPerView: 3,
+                    slidesPerColumn: 2,
+                    paginationClickable: true,
+                    spaceBetween: 30,
+					autoplay: 2500,
+					autoplayDisableOnInteraction: false,
+					breakpoints: {
+						1024: {	slidesPerView: 3, spaceBetween: 40 },
+						768: { slidesPerView: 3, spaceBetween: 30 },
+						640: { slidesPerView: 1, spaceBetween: 20 },
+						320: { slidesPerView: 1, spaceBetween: 10 }
+					}			
+            });
+
+}else{
+	console.log("--> Swiper didn't found");
+}
+
+
+
