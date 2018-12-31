1516e253182a375de9cd2239bdbc989450ec793c
Hussain <gmk.hussain@yahoo.com>
2018-12-31 18:51:21 +0500

    :new: Gulp & NPM package.json added

---
+++ Frontized.md
@@ -0,0 +1,26 @@
+## .colset--slider
+
+```html
+<!-- colset can use with UL LI too. -->
+<section class="colset--slider firstlast-area">
+  <div class="colset  active">Text 1</div>
+  <div class="colset">Text 2</div>
+  <div class="colset">Text 3</div>
+
+  <div class="colset__actions visible-xs">
+    <div class="btn prev disabled">Prev</div>
+    <div class="btn all o4"><i class="glyphicon glyphicon-th-large"></i></div>
+    <div class="btn next">Next</div>
+  </div>
+ </section>
+```
+
+```css
+// SEE CSS
+```
+
+```javascript
+// JS FILE
+```
+
+
---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minifyCss = require('gulp-minify-css');
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const plumber = require('gulp-plumber');
+const pump = require('pump');
+var sourcemaps = require('gulp-sourcemaps');
+var minify = require('gulp-minify');
+
+const minifyjs = require('gulp-js-minify');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+
+
+var log = require('fancy-log');
+const lme = require('lme');
+
+
+const srcPath = 'src';
+const src = {
+  scss: `${srcPath}/scss/main.scss`,
+  html: `${srcPath}/**/*.html`,
+  js: `${srcPath}/js/**/*.js`,
+  img: `${srcPath}/images/**/*`,
+}
+
+
+
+// Dest set to docs for github pages, feel free to change
+const destPath = 'build';
+const dest = {
+  css: `${destPath}/css`,
+  html: destPath,
+  js: `${destPath}/js`,
+  img: `${destPath}/images`,
+
+  css_bundle: `${destPath}/css/*.css`,
+}
+
+
+
+gulp.task('sass', () => {
+  pump([
+    gulp.src(src.scss),
+    sourcemaps.init(),
+    plumber(err => console.error(err)),
+    sass({ style: 'compressed' }).on('error', sass.logError),
+    autoprefixer({ browsers: ['last 2 versions'] }),
+    minifyCss(),
+    sourcemaps.write('./'),
+    gulp.dest(dest.css),
+    browserSync.stream({match: '**/*.css'})
+  ]);
+});
+/*
+gulp.task('html', () => {
+  pump([
+    gulp.src(src.html),
+    plumber(err => console.error(err)),
+    htmlmin({ collapseWhitespace: true, removeComments: true }),
+    gulp.dest(dest.html)
+  ]);
+});
+
+*/
+gulp.task('js', () => {
+  pump([
+    gulp.src(src.js),
+    plumber(err => console.error(err)),
+    babel({ presets: ['es2015', 'es2017'] }),
+    uglify(),
+    gulp.dest(dest.js),
+    browserSync.stream()
+  ]);
+});
+
+gulp.task('image', () => {
+  pump([
+    gulp.src(src.img),
+    plumber(err => console.error(err)),
+    imagemin({ verbose: true }),
+    gulp.dest(dest.img)
+  ]);
+}); 
+
+
+
+gulp.task('prod', function() {
+ 
+
+  var css = gulp.src([`${srcPath}/css/bootstrap.min.css`, `${srcPath}/css/stylized.css`, `${srcPath}/css/swiper.min.css`, `${srcPath}/css/font-awesome.min.css`, `${destPath}/css/style.css`])
+  .pipe(minifyCss())
+  .pipe(concat('style.bundle.css'))
+  .pipe(gulp.dest(`./${destPath}/css`));
+
+
+  var headerjs = gulp.src([`${srcPath}/js/jquery-2.2.4.min.js`, `${srcPath}/js/lazysizes.min.js`])
+    .pipe(concat('header.bundle1.js'))
+  //  .pipe(minify())
+    .pipe(gulp.dest(`./${destPath}/js`));
+  
+
+  var footerjs = gulp.src([`${srcPath}/js/bootstrap.min.js`, `${srcPath}/js/kodeized.js`, `${srcPath}/js/viewportchecker.js`, `${srcPath}/js/swiper.jquery.min.js`, `${srcPath}/js/customized.js`])
+      .pipe(concat('footer.bundle1.js'))
+  //    .pipe(minify())
+      .pipe(gulp.dest(`./${destPath}/js`));
+
+      lme.h(" *** DONE ! *** ");
+ 
+  return merge(headerjs, footerjs, css).on('change', browserSync.stream);
+
+});
+
+
+
+//gulp.task('browserSync', ['sass', 'js', 'html'], () => {
+  gulp.task('browserSync', ['sass', 'prod'], () => {
+    browserSync.init({
+      injectChanges: true,
+      server: `./${destPath}`,
+  });
+  
+
+  //gulp.watch([dest.css_bundle, src.scss, src.js], ['sass', 'prod']);
+  gulp.watch([src.scss], ['sass', 'prod']);
+    //  gulp.watch(src.js, ['js']);
+    //  gulp.watch(src.js).on('change', browserSync.stream);
+    //  gulp.watch(src.html, ['html']);
+    //  gulp.watch(src.html).on('change', browserSync.reload);
+    //  gulp.watch(src.js).on('change', browserSync.reload);
+  });
+
+
+
+gulp.task('default', ['browserSync']);
+
+
+
+// const webp = require('gulp-webp');
+
+// imagemin(['images/*.{jpg,png}'], 'build/images', {	use: [		imageminWebp({quality: 50})	]}).then(() => {	console.log('Images optimized');});
+
+gulp.task('webp', () =>
+  gulp.src([`${destPath}/images/**/*`])
+  .pipe(webp())
+  .pipe(gulp.dest(`./${destPath}/images-webp`))
+);
+
